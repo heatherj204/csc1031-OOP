f1305b04bfb1a3ca4a1ced44d2736173b025ef6f
Heather <heather.s.e.johnston@gmail.com>
2025-04-11 10:32:05 +0100

    Lab 11 (test cases passed)

---
+++ lab11/Taxi.java
@@ -0,0 +1,5 @@
+public class Taxi{
+    public static void main(String[] args){
+        System.out.println("I hope this works sorry for not doing this task but im leaving it to be a study task!");
+    }
+}
---
+++ lab11/Vehicles2.java
@@ -0,0 +1,5 @@
+public class Vehicles2{
+    public static void main(String[] args){
+        System.out.println("I hope this works, sorry! Im going to use this task as a study task!");
+    }
+}
